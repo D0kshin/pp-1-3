e5502fde66e79a2e6ed3fd7dcded074f529b9642
D0kshin <dokshin.zahar@gmail.com>
2025-11-18 03:20:43 +0300

    работает вывод

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -7,22 +7,15 @@ public class Main {
     public static void main(String[] args) {
         // реализуйте алгоритм здесь
         UserServiceImpl userService = new UserServiceImpl();
-        userService.createUsersTable();
-
-
-
         userService.saveUser("Ilya", "Abraamov", (byte) 12);
         userService.saveUser("Adolf", "Abraamov", (byte) 16);
         userService.saveUser("Vasya", "Abraamov", (byte) 20);
         userService.saveUser("Ibragim", "Abraamov", (byte) 39);
         userService.saveUser("Egor", "Abraamov", (byte) 3);
 
-
         for(User user : userService.getAllUsers()){
             System.out.println(user);
         }
-        userService.cleanUsersTable();
-        userService.dropUsersTable();
 
     }
 }
--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -1,10 +1,11 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 
-import javax.persistence.Query;
+
 import java.util.List;
 
 import static jm.task.core.jdbc.util.Util.getSessionFactory;
@@ -18,65 +19,75 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public void createUsersTable() {
-        SessionFactory sessionFactory = getSessionFactory();
-        Session session = sessionFactory.openSession();
-        session.beginTransaction();
-        String sql = """
-                CREATE TABLE IF NOT EXISTS user (
-                id INT NOT NULL AUTO_INCREMENT,
-                name VARCHAR(45) NOT NULL,
-                last_name VARCHAR(45) NOT NULL,
-                age INT(3) NOT NULL,
-                PRIMARY KEY (id));
-                """;
-        session.createNativeQuery(sql).executeUpdate();
-        session.getTransaction().commit();
-        session.close();
+        try(SessionFactory sessionFactory = getSessionFactory()) {
+            Session session = sessionFactory.openSession();
+            session.beginTransaction();
+            String sql = """
+                    CREATE TABLE IF NOT EXISTS user (
+                    id INT NOT NULL AUTO_INCREMENT,
+                    name VARCHAR(45) NOT NULL,
+                    last_name VARCHAR(45) NOT NULL,
+                    age INT(3) NOT NULL,
+                    PRIMARY KEY (id));
+                    """;
+            session.createNativeQuery(sql).executeUpdate();
+            session.getTransaction().commit();
+        } catch (HibernateException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void dropUsersTable() {
-        SessionFactory sessionFactory = getSessionFactory();
-        Session session = sessionFactory.openSession();
-        session.beginTransaction();
-        session.createNativeQuery("DROP TABLE IF EXISTS user").executeUpdate();
-        session.getTransaction().commit();
-        sessionFactory.close();
+        try(SessionFactory sessionFactory = getSessionFactory()) {
+            Session session = sessionFactory.openSession();
+            session.beginTransaction();
+            session.createNativeQuery("DROP TABLE IF EXISTS user").executeUpdate();
+            session.getTransaction().commit();
+        } catch (HibernateException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
-        SessionFactory sessionFactory = getSessionFactory();
-        Session session = sessionFactory.openSession();
-        session.save(new User(name, lastName, age));
-        session.close();
+        try(SessionFactory sessionFactory = getSessionFactory()) {
+            Session session = sessionFactory.openSession();
+            session.save(new User(name, lastName, age));
+        } catch (HibernateException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void removeUserById(long id) {
-        SessionFactory sessionFactory = getSessionFactory();
-        Session session = sessionFactory.openSession();
-        User user = session.get(User.class, id);
-        session.delete(user);
-        session.close();
+        try(SessionFactory sessionFactory = getSessionFactory()) {
+            Session session = sessionFactory.openSession();
+            User user = session.get(User.class, id);
+            session.delete(user);
+        } catch (HibernateException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public List<User> getAllUsers() {
-        SessionFactory sessionFactory = getSessionFactory();
-        Session session = sessionFactory.openSession();
-        List<User> users = session.createQuery("FROM " + User.class.getSimpleName()).list();
-        session.close();
-        return users;
+        try (SessionFactory sessionFactory = getSessionFactory()) {
+            Session session = sessionFactory.openSession();
+            List<User> users = session.createQuery("FROM " + User.class.getSimpleName()).list();
+            return users;
+        }
     }
 
     @Override
     public void cleanUsersTable() {
-        SessionFactory sessionFactory = getSessionFactory();
-        Session session = sessionFactory.openSession();
-        session.beginTransaction();
-        session.createQuery("DELETE FROM " + User.class.getSimpleName()).executeUpdate();
-        session.getTransaction().commit();
-        session.close();
+        try (SessionFactory sessionFactory = getSessionFactory()) {
+            Session session = sessionFactory.openSession();
+            session.beginTransaction();
+            session.createQuery("DELETE FROM " + User.class.getSimpleName()).executeUpdate();
+            session.getTransaction().commit();
+        } catch (HibernateException e) {
+            e.printStackTrace();
+        }
     }
 }
